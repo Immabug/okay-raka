b1b01e1453e0b08a5aeb75c8052ebb1b3c55875a
Atlantis <107106982+Immabug@users.noreply.github.com>
2022-12-02 01:03:24 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+
+const no = document.getElementById(`no`);
+const replace = document.querySelector(`.replacer`);
+let i = 0;
+
+function Noo(){
+    i+= 2;
+    let replacer = (i * 2);
+   
+   no.style.display ="none";
+   no.style.display ="block";
+   no.style.position = "absolute";
+   no.style.top  =`${replacer}em`;
+   if(replacer == 12) {
+    no.style.left  =`${replacer}em`;
+    alert(`dapat lage okay ka`);
+    
+   }
+   else if(replacer == 16) {
+    replacer = replacer - i;
+    no.style.top  =`${replacer}em`;
+    alert(`gahig ulo`);
+   }
+   
+   else if(replacer >= 22) {
+    no.style.top  =`${replacer}em`;
+    console.log(replacer);
+    location.reload();
+   }
+}
+
+function volume(){
+    var sound = document.getElementById("sound");
+    sound.volume = 0.5;
+}
+volume();
